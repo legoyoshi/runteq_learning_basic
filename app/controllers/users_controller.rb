724a2fdbaa8a05b39f362fb8c3b0ec27c6212669
legoyoshi <legoyoshi08@gmail.com>
2020-12-03 15:54:44 +0900

    lintエラーを修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,9 +14,8 @@ class UsersController < ApplicationController
 
   private
 
-    def user_params
-      params.require(:user).permit(:last_name, :first_name, :email, :password,
+  def user_params
+    params.require(:user).permit(:last_name, :first_name, :email, :password,
                                    :password_confirmation)
-    end
-
+  end
 end
