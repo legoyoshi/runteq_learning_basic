11bdc3411e40573522199a836c1667cd2ef103f6
legoyoshi <legoyoshi08@gmail.com>
2020-12-05 14:59:34 +0900

    掲示板作成機能を追加

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -4,7 +4,25 @@ class BoardsController < ApplicationController
     @boards = Board.all.includes(:user)
   end
 
+  def new
+    @board = Board.new
+  end
+
+  def create
+    @board = Board.new(board_params)
+    if @board.save
+      redirect_back_or_to(boards_path, success: (t 'flash.board_create_success'))
+    else
+      flash.now[:danger] = (t 'flash.board_create_failed')
+      render 'new'
+    end
+  end
+
   private
+
+  def board_params
+    params.require(:board).permit(:title, :body).merge(user_id: current_user.id)
+  end
 
   def login_check
     return unless current_user.nil?
