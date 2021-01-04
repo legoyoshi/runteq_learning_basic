f0f632cde97184abc236abed663498a97237d9f8
legoyoshi <legoyoshi08@gmail.com>
2021-01-04 19:31:52 +0900

    ブックマークコントローラのリダイレクト先を修正

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -2,11 +2,11 @@ class BookmarksController < ApplicationController
   def create
     @bookmark = current_user.bookmarks.build(board_id: params[:board_id])
     @bookmark.save!
-    redirect_back_or_to(boards_path, success: t('flash.bookmarked'))
+    redirect_back_or_to(request.referer, success: t('flash.bookmarked'))
   end
 
   def destroy
     current_user.bookmarks.find_by(board_id: params[:board_id]).destroy!
-    redirect_back_or_to(boards_path, success: t('flash.unbookmarked'))
+    redirect_back_or_to(request.referer, success: t('flash.unbookmarked'))
   end
 end
